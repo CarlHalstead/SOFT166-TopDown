6648dada80eda1b873ec9155fe8e794373ad297a
Carl H <11426121+CarlHalstead@users.noreply.github.com>
2019-11-18 17:21:29 +0000

    (Extremely) Basic menu and end scenes

---
+++ Assets/Scripts/_Test/Scenes.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenes : MonoBehaviour
+{
+    public static Scenes Instance = null;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
