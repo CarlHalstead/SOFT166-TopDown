841c34d7c5e8f1cb645ebfcf46bb427287f801b6
Carl Halstead <carl.gfinity@gmail.com>
2019-11-24 19:03:10 +0000

    Rearrange project hierarchy

---
+++ Assets/Scripts/Events/Args/OnStatChangedEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class OnStatChangedEventArgs : EventArgs
+{
+	public readonly int Current = 0;
+	public readonly float CurrentAsPercentage = 0f;
+
+	public OnStatChangedEventArgs(int current, float currentAsPercentage)
+	{
+		Current = current;
+		CurrentAsPercentage = currentAsPercentage;
+	}
+}
