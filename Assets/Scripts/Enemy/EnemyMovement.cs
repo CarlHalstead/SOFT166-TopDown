9e0888354755adbd8633f5273351ef66986274be
Carl Halstead <carl.gfinity@gmail.com>
2019-12-07 20:43:02 +0000

    Fix rock and zombie collision
    This fixes the collision matrix so that the player and rock can collide. This also changes the zombies so that they respect collision physics my moving them using a Rigidbody.

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class EnemyMovement : MonoBehaviour
 {
     [Header("References")]
@@ -10,13 +11,20 @@ public class EnemyMovement : MonoBehaviour
     [SerializeField]
     private float speed = 5f;
 
-    private void Update()
-    {
-        if (target != null)
-            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * 0.01f);
-    }
+	private Rigidbody2D rb;
 
-    public void SetTarget(Transform newTarget)
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	private void Update()
+	{
+		if (target != null)
+			rb.MovePosition(Vector3.MoveTowards(transform.position, target.position, speed * 0.01f));
+	}
+
+	public void SetTarget(Transform newTarget)
     {
         target = newTarget;
     }
