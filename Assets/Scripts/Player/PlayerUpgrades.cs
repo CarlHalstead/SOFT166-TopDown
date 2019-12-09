2580c555c993ab438338ab503cff62b8a1e0a6fb
Carl Halstead <carl.gfinity@gmail.com>
2019-12-09 15:42:31 +0000

    Upgrade Shop - Interface
    This commit includes a fairly primitive interface for the upgrade shop and a button to open and close the shop.

---
+++ Assets/Scripts/Player/PlayerUpgrades.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerUpgrades : MonoBehaviour
+{
+	[Header("References")]
+	[SerializeField]
+	private GameObject canvasUpgradeShop = null;
+
+    private void Update()
+    {
+		if (Input.GetButtonDown("TriggerMenu") == true)
+		{
+			canvasUpgradeShop.SetActive(!canvasUpgradeShop.activeInHierarchy);
+
+			if (canvasUpgradeShop.activeInHierarchy == true)
+				Time.timeScale = 0f;
+			else
+				Time.timeScale = 1f;
+		}
+    }
+	public void OpenShop()
+	{
+		canvasUpgradeShop.SetActive(true);
+		Time.timeScale = 0f;
+	}
+
+	public void CloseShop()
+	{
+		canvasUpgradeShop.SetActive(false);
+		Time.timeScale = 1f;
+	}	
+}
