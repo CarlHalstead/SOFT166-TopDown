50151dcc0fedc4803039b77903f243bce28d6714
Carl Halstead <carl.gfinity@gmail.com>
2019-12-09 22:47:38 +0000

    Implement zombie pathfinding

---
+++ Assets/Scripts/Enemy/EnemyPathFinder.cs
@@ -0,0 +1,32 @@
+﻿using Pathfinding;
+using UnityEngine;
+
+public class EnemyPathFinder : MonoBehaviour
+{
+	[Header("References")]
+	private Transform target;
+
+	private IAstarAI ai;
+
+	private void Awake()
+	{
+		ai = GetComponent<IAstarAI>();
+	}
+
+	private void Update()
+	{
+		if (target != null)
+		{
+			if (ai != null)
+			{
+				ai.destination = target.position;
+				ai.SearchPath();
+			}
+		}
+	}
+
+	public void SetTarget(Transform newTarget)
+	{
+		target = newTarget;
+	}
+}
